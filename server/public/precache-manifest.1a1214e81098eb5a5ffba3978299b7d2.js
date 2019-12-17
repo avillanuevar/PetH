318799c84ea935c61064fc296482f1314bc4cb05
Alfonso Villanueva <villanuevaralfonso@gmail.com>
2019-12-17 11:58:39 +0100

    Deploying

---
+++ server/public/precache-manifest.1a1214e81098eb5a5ffba3978299b7d2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69090e4bc3cafde0a19363f3e70b5fc5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "202919ae9de0cf6750ed",
+    "url": "/static/css/2.32b2cd94.chunk.css"
+  },
+  {
+    "revision": "61480ffdd34f3d62068e",
+    "url": "/static/css/main.d36d3cf5.chunk.css"
+  },
+  {
+    "revision": "202919ae9de0cf6750ed",
+    "url": "/static/js/2.dd95a0b2.chunk.js"
+  },
+  {
+    "revision": "0e711cc5f0f2da2f19af3118286a3810",
+    "url": "/static/js/2.dd95a0b2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "61480ffdd34f3d62068e",
+    "url": "/static/js/main.cc391021.chunk.js"
+  },
+  {
+    "revision": "2bcb8084869f23f72706",
+    "url": "/static/js/runtime-main.88d4f3f8.js"
+  }
+]);
