86a6bbf014abdee57b17603008b5cb948ca8b2ce
Alfonso Villanueva <villanuevaralfonso@gmail.com>
2019-12-17 15:54:36 +0100

    hola

--- client/src/components/reservations/reservationDetails.js
+++ client/src/components/reservations/reservationDetails.js
@@ -1,33 +1,34 @@
 import React, { Component } from "react";
 import ReservationService from "../../service/Reservation.service";
 
-class ReservationDetails extends Component{
-    constructor(){
-        super()
-        this._reservationService=new ReservationService
-        this.state={
-            reservation:{}
-        }
-    }
-    componentDidMount = () => {
-        const reservationId = this.props.match.params.id;
-        console.log(reservationId)
-        this._reservationService.details(reservationId)
-            .then(reservationFromDB => this.setState({ reservation: reservationFromDB.data }))
-            .then(() =>console.log(this.state.reservation.details.home.title))
-            .catch(err => console.log("Error", err))
-    }
-    render(){
-        console.log(this.state.reservation);
-        return (
-          <div>
-            {this.state.reservation!={}&&(
-            <h1>{this.state.reservation.details.home.title}</h1>
-
-            )}
-          </div>
-        );
-    }
-
+class ReservationDetails extends Component {
+  constructor() {
+    super();
+    this._reservationService = new ReservationService();
+    this.state = {
+      reservation: null
+    };
+  }
+  componentDidMount = () => {
+    const reservationId = this.props.match.params.id;
+    console.log(reservationId);
+    this._reservationService
+      .details(reservationId)
+      .then(reservationFromDB =>
+        this.setState({ reservation: reservationFromDB.data })
+      )
+      .then(() => console.log(this.state.reservation.details.home.title))
+      .catch(err => console.log("Error", err));
+  };
+  render() {
+    console.log(this.state.reservation);
+    return (
+      <div>
+        {this.state.reservation ? (
+          <h1>{this.state.reservation.details.home.title}</h1>
+        ) : null}
+      </div>
+    );
+  }
 }
-export default ReservationDetails
+export default ReservationDetails;
---
+++ server/public/precache-manifest.eae2b7975253f871352ec658fac45e42.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99880e5d10fad2b5f6c0dd030148f6fd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "202919ae9de0cf6750ed",
+    "url": "/static/css/2.32b2cd94.chunk.css"
+  },
+  {
+    "revision": "502f555814cc2437b994",
+    "url": "/static/css/main.d36d3cf5.chunk.css"
+  },
+  {
+    "revision": "202919ae9de0cf6750ed",
+    "url": "/static/js/2.dd95a0b2.chunk.js"
+  },
+  {
+    "revision": "0e711cc5f0f2da2f19af3118286a3810",
+    "url": "/static/js/2.dd95a0b2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "502f555814cc2437b994",
+    "url": "/static/js/main.ade60049.chunk.js"
+  },
+  {
+    "revision": "2bcb8084869f23f72706",
+    "url": "/static/js/runtime-main.88d4f3f8.js"
+  }
+]);
